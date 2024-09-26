9900d65f053512b9bb17ff359f2584f65b32708c
david <david.silvap@perficient.com>
2024-09-25 20:58:12 -0500

    feat(coffee): add buyerName

---
+++ WebApplication1/Migrations/20240926015655_AddBuyerName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBuyerName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BuyerName",
+                table: "CoffeeInventory",
+                type: "longtext",
+                nullable: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BuyerName",
+                table: "CoffeeInventory");
+        }
+    }
+}
